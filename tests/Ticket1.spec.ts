185eb8fc6c3497f3b93ee8dcd399e8caf8b21291
Rati Nizharadze <rnizharadze@solvd.com>
2026-06-06 17:53:58 +0400

    folder/file structure change: Files moved from root to test-data

--- tests/Ticket1.spec.ts
+++ tests/Ticket1.spec.ts
@@ -1,5 +1,5 @@
 import {test, expect} from '@playwright/test';
-import {standardUser, lockedOutUser} from "../saucedemoallscenariosdata";
+import {standardUser, lockedOutUser} from "../test-data/saucedemoallscenariosdata";
 import {LoginPage} from "../pages/LoginPage";
 
 test.describe("Login", () => {
