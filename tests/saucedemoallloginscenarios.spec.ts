98aa0d42580a04f146053dc339cb4a8ba0ed447d
Rati Nizharadze <rnizharadze@solvd.com>
2026-06-06 15:08:42 +0400

    Revert "Updated TCs and verified all pass"
    This reverts commit 8d2fee092c52ffb40faa51d0737e40ab4a303ce6.

--- tests/saucedemoallloginscenarios.spec.ts
+++ tests/saucedemoallloginscenarios.spec.ts
@@ -37,11 +37,35 @@ test.describe('Go to login page', () => {
     });
     });
 
-//test('Login with performance glitch user', async ({page}) => {
-//    await page.getByTestId('username').fill(performanceGlitchUser.username);
-//    await page.getByTestId('password').fill(performanceGlitchUser.password);
-//    await page.getByTestId('login-button').click();
-//    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    //test('Login with problem user', async ({page}) => {
+    //    await page.getByTestId('username').fill(problemUser.username);
+    //    await page.getByTestId('password').fill(problemUser.password);
+    //    await page.getByTestId('login-button').click();
+    //   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    //    await expect(page.getByTestId('inventory-item')).toHaveCount(6);
+    //    
+    //    const imageSrcs = await page.locator('.inventory_item_img').evaluateAll(
+    //    imgs => imgs.map(img => img.getAttribute('src'))
+    //    );
+
+    //    const uniqueSrcs = new Set(imageSrcs);
+    //    expect(uniqueSrcs.size).toBe(1);
+    //});
+    //});
+
+    //test('Login with performance glitch user', async ({page}) => {
+    //    await page.getByTestId('username').fill(performanceGlitchUser.username);
+    //    await page.getByTestId('password').fill(performanceGlitchUser.password);
+    //    await page.getByTestId('login-button').click();
+    //    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    //
+//    const startTime = Date.now();
+//    await expect(page.getByTestId('inventory-item')).toHaveCount(6);
+//    const endTime = Date.now();
+//    const loadTime = endTime - startTime;
+//    console.log(`Load time for performance glitch user: ${loadTime} ms`);
+//    expect(loadTime).toBeGreaterThan(5000);
+//});
 
 //test('Login with error user', async ({page}) => {
 //    await page.getByTestId("username").fill(errorUser.username);
@@ -54,3 +78,15 @@ test.describe('Go to login page', () => {
 //    const src = await errorImage.getAttribute('src');
 //    expect(src).toContain('sl-404.jpg');
 //});
+
+//test('Login with visual user', async ({page}) => {
+//    await page.getByTestId('username').fill(visualUser.username);
+//    await page.getByTestId('password').fill(visualUser.password);
+//    await page.getByTestId('login-button').click();
+//    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+//
+//    const inventoryContainer = page.locator('.inventory_container');
+//    await expect(inventoryContainer).toBeVisible();
+//    const backgroundColor = await inventoryContainer.evaluate(el => getComputedStyle(el).backgroundColor);
+//    expect(backgroundColor).toBe('rgb(255, 0, 0)');
+});
